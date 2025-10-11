806939e21e416694574617cdd7a739b306461e5e
Rostyslav Borysenko <strukfit0@gmail.com>
2025-10-11 18:58:24 +0200

    backend: added issues crud

---
+++ backend/src/main/java/com/strukfit/taskmanager/issue/dto/IssueCreateDTO.java
@@ -0,0 +1,22 @@
+package com.strukfit.taskmanager.issue.dto;
+
+import com.strukfit.taskmanager.issue.enums.Priority;
+import com.strukfit.taskmanager.issue.enums.Status;
+
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+import lombok.Data;
+
+@Data
+public class IssueCreateDTO {
+    @NotBlank()
+    @Size(max = 60)
+    private String title;
+
+    @Size(max = 255)
+    private String description;
+
+    private Priority priority;
+    private Status status;
+    private Long projectId;
+}
