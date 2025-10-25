61b5ed3253be3a00512a0a15d30b467cd829bd85
Rostyslav Borysenko <strukfit0@gmail.com>
2025-10-25 12:35:14 +0200

    backend: added workspace create date default sorting

--- backend/src/main/java/com/strukfit/taskmanager/v1/workspace/WorkspaceService.java
+++ backend/src/main/java/com/strukfit/taskmanager/v1/workspace/WorkspaceService.java
@@ -4,6 +4,7 @@ import java.util.List;
 
 import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Service;
 
 import com.strukfit.taskmanager.v1.user.User;
@@ -19,7 +20,8 @@ public class WorkspaceService {
     private WorkspaceMapper workspaceMapper;
 
     public List<Workspace> getAllForUser(User user) {
-        return workspaceRepository.findByUser(user);
+        Sort sort = Sort.by(Sort.Direction.DESC, "createdAt");
+        return workspaceRepository.findByUser(user, sort);
     }
 
     public Workspace getById(Long id, User user) {
